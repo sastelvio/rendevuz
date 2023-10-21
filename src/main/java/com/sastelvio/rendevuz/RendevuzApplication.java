272c5eac98dd1d8498d9654ad61ae9f91e5910f0
sastelvios <sast.serafim@gmail.com>
2023-10-21 23:51:57 +0200

    initial commit

---
+++ src/main/java/com/sastelvio/rendevuz/RendevuzApplication.java
@@ -0,0 +1,13 @@
+package com.sastelvio.rendevuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendevuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendevuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sastelvio/rendevuz/RendevuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sastelvio.rendevuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendevuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
