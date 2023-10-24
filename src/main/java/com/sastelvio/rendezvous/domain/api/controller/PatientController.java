fbd19335a3a1ac33c7f4f4a20a391a95e057c25d
sastelvios <sast.serafim@gmail.com>
2023-10-24 20:41:55 +0200

    Feature: Implement update and delete [Patient]

--- src/main/java/com/sastelvio/rendezvous/domain/api/controller/PatientController.java
+++ src/main/java/com/sastelvio/rendezvous/domain/api/controller/PatientController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +24,23 @@ public class PatientController {
     @GetMapping
     public ResponseEntity<List<Patient>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> findById(@PathVariable Long id){
+        Optional<Patient> optPatient = service.findById(id);
+        return optPatient.map(patient -> ResponseEntity.status(HttpStatus.OK).body(patient)).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping
+    public ResponseEntity<Patient> update(@RequestBody Patient patient){
+        return ResponseEntity.status(HttpStatus.OK).body(service.save(patient));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
