9f049d55aa6fce470c4d0a840392081461096a40
sastelvios <sast.serafim@gmail.com>
2023-11-09 02:06:27 +0100

    Feature: Customize SpringDoc

--- src/main/java/com/sastelvio/rendezvous/RendezvousApplication.java
+++ src/main/java/com/sastelvio/rendezvous/RendezvousApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RendezvousApplication {
 
-	//TODO implement documentation with alternative to Swagger UI
 	public static void main(String[] args) {
 		SpringApplication.run(RendezvousApplication.class, args);
 	}
