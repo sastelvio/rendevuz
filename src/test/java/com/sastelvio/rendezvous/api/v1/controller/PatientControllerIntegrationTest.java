dfa44b3b2f0834b4f353d1fa055fff47c1b8840a
sastelvios <sast.serafim@gmail.com>
2024-05-18 18:22:14 +0200

    commit:180524

---
+++ src/test/java/com/sastelvio/rendezvous/api/v1/controller/PatientControllerIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.sastelvio.rendezvous.api.v1.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
